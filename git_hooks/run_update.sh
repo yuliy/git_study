cc338e116a035885b9c337912ce500fecb8f0590
Yuliy Ayvazyan <yuliy.ayvazyan@aliexpress.ru>
2021-05-04 00:38:19 +0300

    Remove run_update.sh

--- git_hooks/run_update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-git_dir="${HOME}/dev/github/git_study/.git"
-refname="myref"
-oldrev="a3d75d0"
-newrev="894b83b"
-
-GIT_DIR=${git_dir} ${git_dir}/hooks/update ${refname} ${oldrev} ${newrev}
